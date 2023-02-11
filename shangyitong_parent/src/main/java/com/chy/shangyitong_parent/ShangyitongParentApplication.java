191608fffe1cbed1e7daabaf6dca81fec8f7c431
cuihongyuan <1723037666@qq.com>
2023-02-11 15:35:24 +0800

    尚医通后端父目录&引入依赖

---
+++ shangyitong_parent/src/main/java/com/chy/shangyitong_parent/ShangyitongParentApplication.java
@@ -0,0 +1,13 @@
+package com.chy.shangyitong_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangyitongParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangyitongParentApplication.class, args);
+    }
+
+}
---
+++ shangyitong_parent/src/test/java/com/chy/shangyitong_parent/ShangyitongParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chy.shangyitong_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShangyitongParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
