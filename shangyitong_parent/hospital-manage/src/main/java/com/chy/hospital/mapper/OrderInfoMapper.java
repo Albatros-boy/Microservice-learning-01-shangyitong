0dc5e3f689d44b90579b311d1864f7fd751909a0
cuihongyuan <1723037666@qq.com>
2023-02-19 16:13:22 +0800

    医院接口模拟系统

---
+++ shangyitong_parent/hospital-manage/src/main/java/com/chy/hospital/mapper/OrderInfoMapper.java
@@ -0,0 +1,10 @@
+package com.chy.hospital.mapper;
+
+import com.chy.hospital.model.OrderInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
